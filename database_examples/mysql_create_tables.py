d4add8786722f4d99504bd93a02fae8a21dc26ae
jianans.shen <jianans.shen@qunar.com>
2019-02-06 15:55:14 +0800

    Add more mysql examples

---
+++ database_examples/mysql_create_tables.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import mysql.connector
+# from mysql.connector.errors import DatabaseError
+
+try:
+    # 接收参数：user, password, host, port=3306, unix_socket and database
+    # 返回一个MySQLConnection Object
+    mydb = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='Sjn19970508',
+        database='runoob_db'
+    )
+
+    # 创建一个查询
+    myCursor = mydb.cursor()
+
+    myCursor.execute("CREATE TABLE sites (name VARCHAR(255), url VARCHAR(255))")
+except Exception as e:
+    print(e)
---
+++ database_examples/mysql_show_databases.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import mysql.connector
+# from mysql.connector.errors import DatabaseError
+
+try:
+    # 接收参数：user, password, host, port=3306, unix_socket and database
+    # 返回一个MySQLConnection Object
+    mydb = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='Sjn19970508'
+    )
+
+    # 创建一个查询
+    myCursor = mydb.cursor()
+    #
+    # # 执行一条原生的SQL语句，执行结果保存在cmd中，没有返回值
+    # myCursor.execute("select * from city")
+    # # 可以使用fetchall()，获取所有的查询结果集，返回值为一个tuple，每一个元素是一个list
+    # res = myCursor.fetchall()
+    # print(res)
+
+    myCursor.execute("SHOW DATABASES")
+except Exception as e:
+    print(e)
+
+for db in myCursor:
+    print(db)
