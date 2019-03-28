d608335180dfbc185ebbc51210ea300c6e9616b4
shenjianan97 <shenjianan97@gmail.com>
2019-03-28 22:25:17 +0800

    Add dimension and indication

--- GUIs/DataVisualizationWin.py
+++ GUIs/DataVisualizationWin.py
@@ -3,6 +3,8 @@
 from views.UI_DataVisualizationWin import Ui_DataVisualizationWin
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtCore import pyqtSignal
+from libs import GL
+from pandas.api.types import *
 
 
 class DataVisualizationWin(QMainWindow, Ui_DataVisualizationWin):
@@ -15,6 +17,12 @@ class DataVisualizationWin(QMainWindow, Ui_DataVisualizationWin):
         self.table_name = table_name
         print('conf:', self.conf)
         print('table_name:', self.table_name)
+        for index in list(GL.tables_df.dtypes.index):
+            data_type = GL.tables_df.dtypes[index]
+            if is_object_dtype(data_type) or is_datetime64_any_dtype(data_type):
+                self.listWidget_dimension.addItem(index)
+            else:
+                self.listWidget_indicator.addItem(index)
 
     def backClicked(self):
         self.backSignal.emit(self.conf)
